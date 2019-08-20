66c15982bca70e801c0d15329b3116c1ae1876e4
Karina Nowicka <knowicka@gmail.com>
2019-08-20 23:01:32 +0200

    Updates

---
+++ precache-manifest.f78bff508821b092e74f0568c8f5603f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3208a205695d03a3cf083d820d01bc",
+    "url": "/my-react-store-front/index.html"
+  },
+  {
+    "revision": "0e256508f24d2a0be04e",
+    "url": "/my-react-store-front/static/css/main.75422e93.chunk.css"
+  },
+  {
+    "revision": "7cbbc3fb6ee2b173eb24",
+    "url": "/my-react-store-front/static/js/2.60710298.chunk.js"
+  },
+  {
+    "revision": "0e256508f24d2a0be04e",
+    "url": "/my-react-store-front/static/js/main.5d173a58.chunk.js"
+  },
+  {
+    "revision": "ef5afbd28c6d0437ad2c",
+    "url": "/my-react-store-front/static/js/runtime~main.474ce230.js"
+  }
+]);
